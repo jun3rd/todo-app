77c7c28880dea33bd54714cbd42be636f1b2c9a7
Jun Alvarado <jun3rd@gmail.com>
2020-10-13 18:23:30 -0700

    place holders

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
 /* main javascript */
+
+// (2)
+const reducer = () => {
+    console.log('reducer');
+    return {store: 'store info'};
+}
+
+// (1)
+const store = Redux.createStore(reducer, {
+    tasks: []
+})
+
+// (3)
+store.subscribe(() => {
+    document.getElementById('status').innerHTML = 'status';
+})
+
+store.dispatch({
+    addTodo: () => {
+        console.log("store's state?")
+    }
+})
